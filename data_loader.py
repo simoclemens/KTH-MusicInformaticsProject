4d12f3a3e2ffb9af5283f4a57dec5462694c4b3a
aliceanselmi <87475490+AliceAnselmi@users.noreply.github.com>
2023-10-04 10:29:44 +0200

    updated class to extend DataSet, extraction of chromagrams with librosa added (to check)

--- data_loader.py
+++ data_loader.py
@@ -3,17 +3,46 @@ import json
 from torch.utils.data import Dataset, DataLoader
 import librosa
 
-# Define a custom dataset class
-class AudioLoader(DataLoader):
-    def __init__(self, audio_folder, analysis_folder, transform=None):
+class AudioDataset(Dataset):
+    def __init__(self, audio_folder, analysis_folder, transform=None, time_window = 60): 
         self.audio_folder = audio_folder
+        self.audio_file_list = os.listdir(audio_folder)
         self.analysis_folder = analysis_folder
         self.transform = transform
+        self.time_window = time_window # now using 60 seconds as default
 
+        # key labels
         pitches = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
         keys = [pitches[i] + ' major' for i in range(12)] + [pitches[i] + ' minor' for i in range(12)]
         self.key_mapping = {keys[i]: i for i in range(24)}
 
+    def __len__(self):
+        return len(self.file_list)
+    
+    def __getitem__(self, idx):
+        audio_file_name = self.file_list[idx]
+        audio_file_path = os.path.join(self.audio_folder, audio_file_name)
+        audio_features = self.pair_audio_with_features(audio_file_name)
+
+        audio_data, sr = librosa.load(audio_file_path, sr=None)
+        frame_length = int(self.time_window * sr)
+        num_frames = len(audio_data) // frame_length
+
+        chromagrams = []
+        #TODO: check if computation of chromagram is correct or if we're using the long way like in labs
+        for frame_idx in range(num_frames):
+            frame_audio = audio_data[frame_idx * frame_length: (frame_idx + 1) * frame_length]
+            chromagram = librosa.feature.chroma_stft(y=frame_audio, sr=sr, n_chroma=24) # check if this is correct
+            chromagrams.append(chromagram)
+
+        key, bpm = audio_features[audio_file_name]
+        key_label = self.key_to_label(key)
+
+        #also returing bpm?
+        return chromagrams, key_label
+
+
+    # pair each file with its annotations
     def pair_audio_with_features(self, file_name):
         analysis_full_path = os.path.join(self.analysis_folder, file_name) + "_analysis.json"
         key = ""
@@ -24,11 +53,17 @@ class AudioLoader(DataLoader):
             bpm = annotations["tempo"]
             f.close()
 
-        # Return dictionary instance with filename as key and analysis as value
         return {file_name: [key, bpm]}
 
     def key_to_label(self,key):
-        return self.key_mapping.get(key, -1)  # Return -1 if the key is not found in the mapping
+        return self.key_mapping.get(key, -1) 
+    
+class AudioDataLoader(DataLoader):
+    def __init__(self, dataset, batch_size=1, shuffle=False, num_workers=0, collate_fn=None):
+        super().__init__(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, collate_fn=collate_fn)
+
 
 if __name__ == "__main__":
-    loader = AudioLoader("audio/wav", "ac_analysis")
+    #test
+    dataset = AudioDataset("audio/wav", "ac_analysis", time_window=60)
+    dataloader = AudioDataLoader(dataset, batch_size=32, shuffle=False)
