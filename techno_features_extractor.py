c6cb9788ecb7e434255a300984c1325f903c8827
aliceanselmi <87475490+AliceAnselmi@users.noreply.github.com>
2023-10-08 10:03:26 +0200

    Added functions for train and test  data splitting

--- techno_features_extractor.py
+++ techno_features_extractor.py
@@ -1,6 +1,7 @@
 from key_extraction.data_loader import AudioDataLoader, AudioDataset
 import json
 import os
+import random
 
 def complete_audio_file_name(uncomplete_file_name, audios_folder="audio"):
     audios_file_list = os.listdir(audios_folder)
@@ -25,6 +26,33 @@ def extract_techno_audios(genres_file_path, techno_audios_file_path="techno_audi
         f.close()
     return techno_audios
 
+
+def split_train_test_data(data, split_ratio=0.8):
+    num_samples = len(data)
+    num_train_samples = int(num_samples * split_ratio)
+    random.shuffle(data)
+
+    train_data = data[:num_train_samples]
+    test_data = data[num_train_samples:]
+
+    return train_data, test_data
+
+def write_train_test_indexes(train_data, test_data, train_index_file="train_indices.json", test_index_file="test_indices.json"):
+    train_indexes = [dataset.audio_file_list.index(filename) for filename in train_data]
+    test_indexes = [dataset.audio_file_list.index(filename) for filename in test_data]
+
+    train_labels = {filename: dataset.pair_audio_with_features(filename)[filename] for filename in train_data}
+    test_labels = {filename: dataset.pair_audio_with_features(filename)[filename] for filename in test_data}
+
+    train_data_dict = {"indices": train_indexes, "labels": train_labels}
+    test_data_dict = {"indices": test_indexes, "labels": test_labels}
+
+    with open(train_index_file, "w") as f:
+        json.dump(train_data_dict, f)
+
+    with open(test_index_file, "w") as f:
+        json.dump(test_data_dict, f)
+
 def write_techno_features_to_file(techno_audios, dataset, techno_features_file_name = "techno_audios_features.json"):
     audio_files_list = dataset.audio_file_list
     techno_audios_features= []
@@ -45,3 +73,5 @@ if __name__ == "__main__":
     dataloader = AudioDataLoader(dataset, batch_size=32, shuffle=False)
     techno_audios = extract_techno_audios("parent_genres.json", "techno_audios.json")
     write_techno_features_to_file(techno_audios, dataset)
+    train_data, test_data = split_train_test_data(techno_audios)
+    write_train_test_indexes(train_data, test_data)
