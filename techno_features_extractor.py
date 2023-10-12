a8c231a10e14c90ef8db477c420305709ae8e11c
aliceanselmi <87475490+AliceAnselmi@users.noreply.github.com>
2023-10-12 10:26:55 +0200

    Moved json files to new folder, generated train and test indeces

--- techno_features_extractor.py
+++ techno_features_extractor.py
@@ -16,6 +16,24 @@ def complete_audio_file_name(uncomplete_file_name):
         if uncomplete_file_name in file_name:
             return file_name
     
+
+def get_audio_duration_in_seconds(filename):
+    file_path = os.path.join(audio_path, filename + '.wav.wav')
+    duration = -1
+    if os.path.exists(file_path):
+        duration = AudioDataset.get_audio_duration(file_path)
+    return duration
+
+def split_train_test_data(data, split_ratio=0.8):
+    num_samples = len(data)
+    num_train_samples = int(num_samples * split_ratio)
+    random.shuffle(data)
+
+    train_data = data[:num_train_samples]
+    test_data = data[num_train_samples:]
+
+    return train_data, test_data
+
 def write_techno_audios(genres_file_path, techno_audios_file_path="techno_audios.json"):
     with open(genres_file_path) as f:
         audios_with_genres = json.load(f)
@@ -35,26 +53,6 @@ def write_techno_audios(genres_file_path, techno_audios_file_path="techno_audios
         f.close()
     return techno_audios
 
-
-def split_train_test_data(data, split_ratio=0.8):
-    num_samples = len(data)
-    num_train_samples = int(num_samples * split_ratio)
-    random.shuffle(data)
-
-    train_data = data[:num_train_samples]
-    test_data = data[num_train_samples:]
-
-    return train_data, test_data
-
-
-def get_audio_duration_in_seconds(filename):
-    file_path = os.path.join(audio_path, filename + '.wav.wav')
-    duration = -1
-    if os.path.exists(file_path):
-        duration = AudioDataset.get_audio_duration(file_path)
-    return duration
-
-
 def write_selected_techno_audios_with_duration(techno_audios, sel_techno_audios_file = "selected_techno_audios.json", techno_audios_with_duration_file="techno_audios_with_duration.json"):
     selected_techno_audios_with_duration = []
     for audio in techno_audios:
@@ -73,16 +71,26 @@ def write_selected_techno_audios_with_duration(techno_audios, sel_techno_audios_
 
 
 def write_train_test_indices(train_data, test_data, train_index_file="train_indices.json", test_index_file="test_indices.json"):
+    print("Train data: ", len(train_data), "Test data: ", len(test_data))
     train_indexes = [dataset.audio_file_list.index(filename) for filename in train_data]
     test_indexes = [dataset.audio_file_list.index(filename) for filename in test_data]
 
-    labels = [{"duration_seconds": get_audio_duration_in_seconds(filename)
-               for filename, index in zip(dataset.audio_file_list, range(len(dataset.audio_file_list)))},
-              {"key": dataset.pair_audio_with_features(filename)[filename][0] for filename, index in zip(dataset.audio_file_list, range(len(dataset.audio_file_list)))},
-              {"bpm": dataset.pair_audio_with_features(filename)[filename][1] for filename, index in zip(dataset.audio_file_list, range(len(dataset.audio_file_list)))}]
+    train_labels = [{"filename": file,
+                    "duration_seconds": get_audio_duration_in_seconds(file),
+                    "key": dataset.pair_audio_with_features(file)[file][0], 
+                    "bpm": dataset.pair_audio_with_features(file)[file][1]
+                    } for file in train_data
+                    ]
     
-    train_data_dict = {"indexes": train_indexes, "labels": labels}
-    test_data_dict = {"indexes": test_indexes, "labels": labels}
+    test_labels = [{"filename": file,
+                    "duration_seconds": get_audio_duration_in_seconds(file),
+                    "key": dataset.pair_audio_with_features(file)[file][0], 
+                    "bpm": dataset.pair_audio_with_features(file)[file][1]
+                    } for file in test_data
+                    ]
+    
+    train_data_dict = {"indexes": train_indexes, "labels": train_labels}
+    test_data_dict = {"indexes": test_indexes, "labels": test_labels}
     with open(train_index_file, "w") as f:
         json.dump(train_data_dict, f)
 
@@ -103,10 +111,10 @@ if __name__ == "__main__":
     dataset = AudioDataset(audio_path, analysis_path, time_window=time_window)
     dataloader = AudioDataLoader(dataset, batch_size=32, shuffle=False)
     techno_audios = write_techno_audios("parent_genres.json", "techno_audios.json")
-    selected_techno_audios = write_selected_techno_audios_with_duration(techno_audios, "selected_techno_audios.json")
+    selected_techno_audios = write_selected_techno_audios_with_duration(techno_audios)
     print(len(selected_techno_audios))
 
-    # write_techno_features_to_file(selected_techno_audios)
-    # train_data, test_data = split_train_test_data(selected_techno_audios)
-    # write_train_test_indices(train_data, test_data)
+    write_techno_features_to_file(selected_techno_audios)
+    train_data, test_data = split_train_test_data(selected_techno_audios)
+    write_train_test_indices(train_data, test_data)
 
